29031a8f1e5506847154d238a3ccaf67179adf90
Darsheen <105065934+Darsheen@users.noreply.github.com>
2022-05-06 21:58:43 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CreateTicket from './container/CreateTicket';
+
+function App() {
+  return (
+    <div >
+      <CreateTicket></CreateTicket>
+    </div>
+  );
+}
+
+export default App;
