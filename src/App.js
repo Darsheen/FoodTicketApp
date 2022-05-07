21f2ac730ea6e91ff62127b6a7e9cc90c63c7806
Darsheen <105065934+Darsheen@users.noreply.github.com>
2022-05-07 08:17:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import CreateTicket from './container/CreateTicket';
+import Graphics from './container/Graphics';
 
 function App() {
   return (
     <div >
       <CreateTicket></CreateTicket>
+      {/* <Graphics></Graphics> */}
     </div>
   );
 }
